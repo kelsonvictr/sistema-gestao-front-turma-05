748b66fb6fd196fdcd3093d54c9aa54a389005bd
Kelson Almeida <kelson.victr@gmail.com>
2026-02-28 10:27:38 -0300

    feat: implement routing in App component and add Inicial page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Inicial from './pages/Inicial'
+import Fornecedores from './pages/Fornecedores'
 import CadastrarFornecedor from './pages/CadastrarFornecedor'
+
 
 const App = () => {
   return (
-    <div>
-      <CadastrarFornecedor />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicial />} />
+        <Route path="/fornecedores" element={<Fornecedores />} />
+        <Route path="/cadastrar-fornecedor" element={<CadastrarFornecedor />} /> 
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Inicial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Inicial = () => {
+  return (
+    <div>
+        <h1>Em manutenção!</h1>
+    </div>
+  )
+}
+
+export default Inicial
